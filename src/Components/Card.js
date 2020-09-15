2a2acb09f2ba6ed31eebd5d59ef4ea64ccdd3b02
Teodora3120 <vestale.teodora00@gmail.com>
2020-09-15 17:51:44 +0300

    Added Modals on click

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,15 +1,48 @@
 import React from 'react';
+import {Modal, Button, ModalHeader} from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Card = ({id, name, username, email}) =>{
-     return <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-         <img alt='card person' src = {`https://robohash.org/${id}?200x200`} />
+class Card extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            modalToggler: false 
+        }
+    }
+    
+    toggler = () => {
+        return this.setState({modalToggler: !this.state.modalToggler})
+    }
+   render() {
+     return <div onClick = {() => this.setState({modalToggler: true})} className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+     
+     <div>
+        <Modal isOpen={this.state.modalToggler} toggle={this.toggler} style={{borderRadius: 48}}>
+        <ModalHeader className={'bg-light-green'}>
+            <Button onClick = {() => this.setState({modalToggler: false})} style = {{alignSelf: 'flex-start', width: '50px', background: 'none', border: 'none', outline: 'none', color: 'black'}}>X</Button>
+        </ModalHeader>
+           <div className="tc bg-light-green dib br3 bw2 shadow-5">
+           <img alt='card person' src = {`https://robohash.org/${this.props.id}?200x200`} />
+            <div>
+                <h2>{this.props.name}</h2>
+                <h3>{this.props.username}</h3>
+                <p>{this.props.email}</p>
+            </div>
+            <div className = {'tc mb-3'}>
+                <Button color="info" onClick = {()=> window.open(`https://robohash.org/${this.props.id}`, '_blank')}>
+                    Visit me on the web
+                </Button>
+            </div>
+           </div>
+      </Modal>
+        </div>
+         
+         <img alt='card person' src = {`https://robohash.org/${this.props.id}?200x200`} />
          <div>
-             <h2>{name}</h2>
-             <h3>{username}</h3>
-             <p>{email}</p>
+             <h2>{this.props.name}</h2>
          </div>
      </div>
 }
-
+}
 
 export default Card;
